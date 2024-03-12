8694fa147cfd19e8f2aac4f3f950ed61b014e4dc
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2024-03-12 11:07:19 -0300

    feat: Add 'install_shellpack'

---
+++ src/scripts/install_shellpack.bash
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+###
+# Script installs shellpack.
+#
+# Example with defaults:
+#
+#   ./src/scripts/install_shellpack.bash
+#
+# Examples with params specified:
+#
+#  VERSION=dev INSTALL_DIR=~/bin ./src/scripts/install_shellpack.bash
+#  VERSION=2469670269b79a4a47975fb8ae1ef68fc9dd09e0 INSTALL_DIR=~/bin ./src/scripts/install_shellpack.bash
+#
+###
+
+set -euo pipefail
+
+DEBUG=${DEBUG:-0}
+[ "${DEBUG}" = 1 ] && set -x
+
+GREEN=$(printf '\033[32m')
+YELLOW=$(printf '\033[33m')
+NC=$(printf '\033[0m')
+
+NAME="shellpack"
+CMD_NAME="shellpack"
+
+# $1 - expected path
+path_in_path() {
+  local dir="$1"
+  if echo "$PATH" | tr ':' '\n' | grep -qx "$dir"; then
+    return 0 # true
+  else
+    return 1 # false
+  fi
+}
+
+# $1 - version
+# $2 - install directory (optional)
+shellpack_install() {
+  printf "${GREEN}%s${NC}\n" "Installation started."
+  local version=${1}
+  local install_dir=${2:-"$(mktemp -d)"}
+  # eval to resolve ~ in the path
+  eval install_dir="${install_dir}"
+  mkdir -p "${install_dir}"
+  # install
+  curl -s https://raw.githubusercontent.com/rynkowsg/shellpack/${version}/main/src/pl/rynkowski/shellpack.cljc -o "${install_dir}/${CMD_NAME}"
+  chmod +x "${install_dir}/${CMD_NAME}"
+  printf "${GREEN}%s${NC}\n" "${NAME} installed in ${install_dir}."
+  # update PATH (if needed)
+  if ! path_in_path "${install_dir}"; then
+    export PATH="${install_dir}:${PATH}"
+    printf "${GREEN}%s${NC}\n" "Updated PATH with ${install_dir}."
+  fi
+}
+
+DEFAULT_INSTALL_DIR=~/bin
+DEFAULT_VERSION="main"
+# https://github.com/rynkowsg/shellpack
+
+detect_required_version() {
+  # if version required specified take it
+  # otherwise try parse from .tool-versions
+  # or try default
+  local version log_message
+  if [ -n "${VERSION+x}" ] && [ -n "${VERSION}" ]; then
+    version="${VERSION}"
+    log_message="$(printf "%s\n" "detected the requested version specified: ${version}")"
+  elif [ -f .tool-versions ]; then
+    version=$(grep -m1 "^${ASDF_ENTRY_NAME} " .tool-versions | awk '{print $2}')
+    log_message="$(printf "%s\n" "detected version defined in .tools-versions: ${version}")"
+  else
+    version="${DEFAULT_VERSION}"
+    log_message="$(printf "${YELLOW}%s${NC}\n" "failed to detect desirable version, taking default: ${version}")"
+  fi
+  1>&2 printf "%s\n" "${log_message}"
+  echo "${version}"
+}
+
+detect_install_dir() {
+  local install_dir
+  if [ -z "${INSTALL_DIR+x}" ]; then
+    # INSTALL_DIR is not declared - take default value
+    install_dir="${DEFAULT_INSTALL_DIR}"
+  elif [ -z "${INSTALL_DIR}" ]; then
+    # INSTALL_DIR is declared but empty" - use temporary directory
+    install_dir="$(mktemp -d)}"
+  else
+    # INSTALL_DIR is declared and not empty - use the given value
+    install_dir="${INSTALL_DIR}"
+  fi
+  echo "${install_dir}"
+}
+
+function main {
+  local version install_dir
+  version="$(detect_required_version)"
+  install_dir="$(detect_install_dir)"
+  shellpack_install "${version}" "${install_dir}"
+}
+
+# shellcheck disable=SC2199
+# to disable warning about concatenation of BASH_SOURCE[@]
+# It is not a problem. This part pf condition is only to prevent `unbound variable` error.
+if [[ -n "${BASH_SOURCE[@]}" && "${BASH_SOURCE[0]}" != "${0}" ]]; then
+  [[ -n "${BASH_SOURCE[0]}" ]] && printf "%s\n" "Loaded: ${BASH_SOURCE[0]}"
+else
+  main "$@"
+fi
---
+++ test/commands/test_command_install_shellpack.bats
@@ -0,0 +1,29 @@
+#!/usr/bin/env bats
+
+###
+# Bats script validating orb command install_babashka.
+#
+#  INSTALL_DIR=~/bin ./test/command/test_command_install_shellpack.bash
+###
+
+# detect ROOT_DIR - BEGIN
+TEST_DIR="$(cd "$(dirname "${BATS_TEST_FILENAME}")" || exit 1; pwd -P)"
+ROOT_DIR="$(cd "${TEST_DIR}/../.." || exit 1; pwd -P)"
+# detect ROOT_DIR - end
+
+setup() {
+  source "${ROOT_DIR}/src/scripts/install_shellpack.bash" # CMD_NAME
+}
+
+@test "Can be found in PATH and run" {
+  $CMD_NAME
+}
+
+@test "File exists in INSTALL_DIR" {
+  if [[ -v INSTALL_DIR && -z $INSTALL_DIR ]]; then
+    echo "skip this test"
+    echo "when user sets INSTALL_DIR= the script takes temp directory for installation"
+  else
+    find "${INSTALL_DIR}" -type f -name "${CMD_NAME}" -print -quit | grep -q .
+  fi
+}
