6e0471600b29d3f8b7139888de9918987ccb94f8
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2024-04-02 15:40:52 -0300

    feat: Add command 'os_detect'

---
+++ src/scripts/detect_os.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+case $(uname) in
+  [Ll]inux*)
+    if [ -f /.dockerenv ]; then
+      echo 'export EXECUTOR=docker' >> $BASH_ENV
+    else
+      echo 'export EXECUTOR=linux' >> $BASH_ENV
+    fi
+    ;;
+  [Dd]arwin*)
+    echo 'export EXECUTOR=macos' >> $BASH_ENV
+    ;;
+  msys*|MSYS*|nt|win*)
+    echo 'export EXECUTOR=windows' >> $BASH_ENV
+    ;;
+esac
+source $BASH_ENV
+
+# Source:
+# https://discuss.circleci.com/t/circleci-os-detect-replacement/46639/2
+#
+# Similar
+# https://github.com/CircleCI-Archived/os-detect-orb/blob/master/src/commands/init.yml#L7
